a6c0a660b373b70d792d77d141211f6dde846e18
newt0 <py_lambda@yahoo.co.jp>
2020-07-01 22:24:51 +0900

    chap3/Add a couple of link to pages.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
   return (
     <div>
       <h1>About Me</h1>
       <p>I'm me.</p>
+      <p><Link to="/contact">Contact</Link></p>
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,7 @@ const ContactPage = () => {
   return (
     <div>
       <h1>Contact</h1>
-      <p>The best way to reach me is via @kyohei?ai on Twitter!</p>
+      <p>The best way to reach me is via <a href="https://twitter.com/kyohei_ai" target="_blank">@kyohei_ai</a>on Twitter!</p>
     </div>
   )
 }
