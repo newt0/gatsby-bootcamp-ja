95a85f42873142b3a27783b27c91a4b7c36b4c8d
newt0 <py_lambda@yahoo.co.jp>
2020-07-01 22:49:48 +0900

    chap4/Create a shared page header

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Footer from '../components/footer'
+import Header from '../components/header'
 
 const AboutPage = () => {
   return (
     <div>
+      <Header />
       <h1>About Me</h1>
       <p>I'm me.</p>
       <p><Link to="/contact">Contact</Link></p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Footer from '../components/footer'
+import Header from '../components/header'
 
 const IndexPage = () => {
   return (
     <div>
+      <Header />
       <h1>Hello.</h1>
       <h2>I'm Kyohei, a web developer living in Tokyo.</h2>
       <p>Need a developer? <a href="/contact">Contact me.</a></p>
