c7980787e87d035b8b463719e1c4d489d2e96698
newt0 <py_lambda@yahoo.co.jp>
2020-07-01 22:50:19 +0900

    del: a hrefの方のContactを削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ const IndexPage = () => {
       <Header />
       <h1>Hello.</h1>
       <h2>I'm Kyohei, a web developer living in Tokyo.</h2>
-      <p>Need a developer? <a href="/contact">Contact me.</a></p>
       <p>Need a developer?<Link to="/contact">Contact me.</Link></p>
       <Footer />
     </div>
