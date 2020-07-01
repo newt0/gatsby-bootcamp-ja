2d3d822e4010cedd20025a96100be3ebce0d8e15
newt0 <py_lambda@yahoo.co.jp>
2020-07-01 22:18:45 +0900

    chap3/Linking Between Pages with Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello.</h1>
       <h2>I'm Kyohei, a web developer living in Tokyo.</h2>
+      <p>Need a developer? <a href="/contact">Contact me.</a></p>
+      <p>Need a developer?<Link to="/contact">Contact me.</Link></p>
     </div>
   )
 }
