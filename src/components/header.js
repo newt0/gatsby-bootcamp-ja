a2ba5a001e7b12f33dec5f9648562d05eb11c486
newt0 <py_lambda@yahoo.co.jp>
2020-07-02 00:11:35 +0900

    7. Styling Gatsby with CSS Modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import headerStyles from './header.module.scss'
+
 const Header = () => {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link className={headerStyles.title} to="/">
+          Kyohei
+        </Link>
+      </h1>
       <nav>
-        <ul>
+        <ul className={headerStyles.navList}>
           <li>
-            <Link to="/">Home</Link>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link className={headerStyles.navItem} to="/blog">Blog</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link className={headerStyles.navItem} to="/about">About</Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link className={headerStyles.navItem} to="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
