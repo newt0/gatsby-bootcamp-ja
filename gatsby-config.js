00ae6f1fd3f765f063c88453d90bfce94830bbef
newt0 <py_lambda@yahoo.co.jp>
2020-07-02 11:09:30 +0900

    10. Sourcing Content from the File System

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
     author: 'newt0'
   },
   plugins: [
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    }
   ]
 }
---
+++ src/posts/gatsby.md
@@ -0,0 +1,12 @@
+---
+title: "The Great Gatsby Bootcamp"
+date: "2019-04-04"
+---
+
+I just launhed a new bootcamp!
+
+## Topics Covered
+
+1. Gatsby
+2. React
+3. GraphQL
